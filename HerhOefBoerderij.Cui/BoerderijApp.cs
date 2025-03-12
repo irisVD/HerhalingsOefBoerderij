6e336eee7fb6d7c62a33f7805a3e19604d35d111
Iris Van Damme <iris.vandamme@student.hogent.be>
2025-03-12 02:54:55 +0100

    Added Akker domain rules

--- HerhOefBoerderij.Cui/BoerderijApp.cs
+++ HerhOefBoerderij.Cui/BoerderijApp.cs
@@ -8,13 +8,15 @@ public class BoerderijApp
 {
     public void Run(){
         Console.WriteLine("boerderij");
-        Eigendom eigendom = new Akker("Achteraan", 200000, 4000, GewasEnum.Aardappel);
+        Eigendom akker = new Akker("Achteraan", 200000, 4000, GewasEnum.Aardappel);
         Persoon persoon = new Persoon("Ben",DateTime.Parse("13-03-2019"));
-        Boerderij boerderij = new Boerderij("boerderij1", "45349255647", new List<Persoon>{{persoon}}, new List<Eigendom>{{eigendom}});
+        Boerderij boerderij = new Boerderij("boerderij1", "45349255647", new List<Persoon>{{persoon}}, new List<Eigendom>{{akker}});
 
-        System.Console.WriteLine(eigendom);
+        System.Console.WriteLine(akker);
 
         Eigendom melkerij = new Melkerij("Melkerij1", 1200, 13);
         System.Console.WriteLine("Opbrengst melkerij: " + melkerij.BerekenOpbrengst());
+
+        System.Console.WriteLine("Opbrengst akker: " + akker.BerekenOpbrengst());
     }
 }
--- HerhOefBoerderij.Domein/Akker.cs
+++ HerhOefBoerderij.Domein/Akker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 
 namespace HerhOefBoerderij.Domein;
 
@@ -6,14 +7,28 @@ public class Akker : Eigendom
 {
     public Akker(string naam, double kost, double oppervlaketeM2, GewasEnum gewas) : base(naam, kost)
     {
+        if (oppervlaketeM2 <= 0)
+            throw new ArgumentException();
+        else
+            OppervlakteM2 = oppervlaketeM2;
         OppervlakteM2 = oppervlaketeM2;
         Gewas = gewas;
     }
 
-    public double OppervlakteM2 { get; set; }
+    public readonly double OppervlakteM2;
     public GewasEnum Gewas { get; set; }
 
-    public override double BerekenOpbrengst(){
-        throw new NotImplementedException();
+    public override double BerekenOpbrengst()
+    {
+        switch (Gewas)
+        {
+            case GewasEnum.Aardappel:
+                return OppervlakteM2 * 0.32;
+            case GewasEnum.Prei:
+                return OppervlakteM2 * 0.32;
+            case GewasEnum.Hop:
+                return OppervlakteM2 * 0.32;
+        }
+        throw new Exception("Gewas bestaat niet.");
     }
 }
